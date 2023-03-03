41cdf57c69270ee6b9e492e84ccf90d60af9730a
AlexMachariaMaranga <amaranga22@gmail.com>
2023-03-03 10:13:11 +0300

    removed faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-require 'faker'
+# Creates fake data to dataabase
+# require 'faker'
 
 
 puts "Deleting old data..."
@@ -7,9 +8,12 @@ User.destroy_all
 Review.destroy_all
 
 puts "Creating users..."
-user1 = User.create(name: Faker::Name.name)
-user2 = User.create(name: Faker::Name.name)
-user3 = User.create(name: Faker::Name.name)
+# user1 = User.create(name: Faker::Name.name)
+# user2 = User.create(name: Faker::Name.name)
+# user3 = User.create(name: Faker::Name.name)
+user1 = User.create(name: "Kinuzs 1")
+user2 = User.create(name: "Kinuzs 2")
+user3 = User.create(name: "Kinuzs 3")
 
 puts "Creating products..."
 product1 = Product.create(name: "Staples", price: 10)
@@ -24,7 +28,6 @@ review1 = Review.create(star_rating: 5, comment: "Good product", user_id: user1.
 review2 = Review.create(star_rating: 4, comment: "It broke in a day", user_id: user1.id, product_id: product2.id)
 review3 = Review.create(star_rating: 2, comment: "Awsome", user_id: user2.id, product_id: product4.id)
 review4 = Review.create(star_rating: 3, comment: "Satisfied", user_id: user2.id, product_id: product3.id)
-
 
 
 puts "completed🌱"
